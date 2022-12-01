027f838e8bd69adc786cca0d4dcb24b96f9c0adc
장윤성 <dbstjd1014@naver.com>
2022-12-01 20:48:34 +0900

    12/01 백준1001

---
+++ Alogorithm/src/class1/Bj_1001_1201.java
@@ -0,0 +1,20 @@
+package class1;
+/* 
+ * 2022/12/01
+ * 문제 : 두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성
+ */
+
+import java.util.Scanner;
+
+public class Bj_1001_1201 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		System.out.println(a-b);
+		
+	}
+}
