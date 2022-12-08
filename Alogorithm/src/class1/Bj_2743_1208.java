4fee96be6230026e3ec79b89e5452566e9f67883
장윤성 <dbstjd1014@naver.com>
2022-12-08 21:34:49 +0900

    12/08 백준2743

---
+++ Alogorithm/src/class1/Bj_2743_1208.java
@@ -0,0 +1,21 @@
+package class1;
+/* 
+ * 2022/12/08
+ * 문제 : 알파벳으로만 이루어진 단어를 입력받아, 그 길이를 출력하는 프로그램을 작성하시오.
+ */
+import java.util.Scanner;
+
+public class Bj_2743_1208 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String s = sc.next();
+		
+		//문자열의 길이 가져오기
+		System.out.println(s.length());
+		
+		
+	}
+
+}
