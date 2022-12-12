058d2c6489448cb020f607d2d982ede12b1fc188
장윤성 <dbstjd1014@naver.com>
2022-12-12 23:21:48 +0900

    12/12 백준5337

--- Alogorithm/src/class1/Bj_5337_1212.java
+++ Alogorithm/src/class1/Bj_5337_1212.java
@@ -1,13 +1,16 @@
 package class1;
 /* 
  * 2022/12/12
- * 문제 : 예제 Welcome을 출력하시오
+ * 문제 : 마이크로소프트 로고 출력
  */
 
 public class Bj_5337_1212 {
 
 	public static void main(String[] args) {
-		
-		System.out.println(".  .   .\n" + "|  | _ | _. _ ._ _  _\n" + "|/\\|(/.|(_.(_)[ | )(/.\n");
+		System.out.printf("       _.-;;-._\n");
+		System.out.printf("'-..-'|   ||   |\n");
+		System.out.printf("'-..-'|_.-;;-._|\n");
+		System.out.printf("'-..-'|   ||   |\n");
+		System.out.printf("'-..-'|_.-''-._|");
 	}
 }
