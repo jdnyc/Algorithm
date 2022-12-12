b1f46ea95d303fafc79a97baee0eac86446c070b
장윤성 <dbstjd1014@naver.com>
2022-12-12 23:24:46 +0900

    12/12 백준5337

--- Alogorithm/src/class1/Bj_5337_1212.java
+++ Alogorithm/src/class1/Bj_5337_1212.java
@@ -1,16 +1,21 @@
 package class1;
 /* 
  * 2022/12/12
- * 문제 : 마이크로소프트 로고 출력
+ * 문제 : 콜센터 직원 출력
  */
 
 public class Bj_5337_1212 {
 
 	public static void main(String[] args) {
-		System.out.printf("       _.-;;-._\n");
-		System.out.printf("'-..-'|   ||   |\n");
-		System.out.printf("'-..-'|_.-;;-._|\n");
-		System.out.printf("'-..-'|   ||   |\n");
-		System.out.printf("'-..-'|_.-''-._|");
+		System.out.printf("     /~\\\n");
+		System.out.printf("    ( oo|\n");
+		System.out.printf("    _\\=/_\n");
+		System.out.printf("   /  _  \\\n");
+		System.out.printf("  //|/.\\|\\\\\n");
+		System.out.printf(" ||  \\ /  ||\n");
+		System.out.printf("============\n");
+		System.out.printf("|          |\n");
+		System.out.printf("|          |\n");
+		System.out.printf("|          |");
 	}
 }
