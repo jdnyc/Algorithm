4223b7b09914936565e9abada4c0c6479e519152
장윤성 <dbstjd1014@naver.com>
2022-12-12 23:19:35 +0900

    12/12 백준5337

---
+++ Alogorithm/src/class1/Bj_5337_1212.java
@@ -0,0 +1,13 @@
+package class1;
+/* 
+ * 2022/12/12
+ * 문제 : 예제 Welcome을 출력하시오
+ */
+
+public class Bj_5337_1212 {
+
+	public static void main(String[] args) {
+		
+		System.out.println(".  .   .\n" + "|  | _ | _. _ ._ _  _\n" + "|/\\|(/.|(_.(_)[ | )(/.\n");
+	}
+}
