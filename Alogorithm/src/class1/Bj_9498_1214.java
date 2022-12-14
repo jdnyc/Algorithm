7541fc271788c998ac9d77d06541f6810a81a055
장윤성 <dbstjd1014@naver.com>
2022-12-14 20:57:06 +0900

    12/14 백준9498

--- Alogorithm/src/class1/Bj_9498_1214.java
+++ Alogorithm/src/class1/Bj_9498_1214.java
@@ -13,16 +13,22 @@ public class Bj_9498_1214 {
 		
 		int score = sc.nextInt();
 		
+		//점수는 0에서 100사이의 정수
 		if(score>=0&&score<=100) {
 			if(score>=90&&score<=100) {
+				//90이상 100이하면 A 출력
 				System.out.println("A");
 			} else if(score>=80&&score<=89) {
+				//80이상 89이하면 B출력
 				System.out.println("B");
 			} else if(score>=70&&score<=79) {
+				//70이상 79이하면 C출력
 				System.out.println("C");
 			} else if(score>=60&&score<=69) {
+				//60이상 69이하면 D출력
 				System.out.println("D");
 			} else {
+				//나머지 점수 F 출력
 				System.out.println("F");
 			}
 		}
