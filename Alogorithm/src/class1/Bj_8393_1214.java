fa39187edd6ed03a31a7da1b8856d2787baeb52b
장윤성 <dbstjd1014@naver.com>
2022-12-14 20:38:17 +0900

    12/14 백준8983

---
+++ Alogorithm/src/class1/Bj_8393_1214.java
@@ -0,0 +1,20 @@
+package class1;
+/* 
+ * 2022/12/14
+ * 문제 : n이 주어졌을 때, 1부터 n까지 합을 구하는 프로그램을 작성하시오.
+ */
+import java.util.Scanner;
+public class Bj_8393_1214 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int sum = 0;
+		
+		for(int i=1; i<=n; i++) {
+			sum += i;
+		}
+		System.out.println(sum);
+	}
+}
